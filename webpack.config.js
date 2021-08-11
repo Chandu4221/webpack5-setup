5d276a30cb3c7deb79d552c697caa3737d90ebd0
dchandra <chndraja@gmail.com>
2021-08-11 23:46:21 +0530

    webpack5 setup --in progress

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { ProvidePlugin } = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: path.resolve(__dirname, "src/js/script.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].[contenthash].js",
+    clean: true,
+  },
+  // dev server
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    overlay: true,
+    watchContentBase: true,
+    open: true,
+  },
+  // loaders
+  module: {
+    rules: [
+      // ejs
+      {
+        test: /\.ejs$/,
+        loader: "ejs-loader",
+        options: {
+          variable: "data",
+          interpolate: "\\{\\{(.+?)\\}\\}",
+          evaluate: "\\[\\[(.+?)\\]\\]",
+        },
+      },
+      // css
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+              sassOptions: {
+                includePaths: ["./node_modules"],
+              },
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      // js
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  // plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "!!ejs-compiled-loader!./src/index.ejs",
+      filename: "index.html",
+      title: "Webpack 5 Setup",
+      data: "Item",
+    }),
+    new ProvidePlugin({
+      _: "underscore",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
+  ],
+};
